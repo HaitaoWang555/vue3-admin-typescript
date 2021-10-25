dd39feb52cf73190fc9d26c2157fd38ea14e275d
wanghaitao <15124505701@163.com>
2021-10-25 23:23:15 +0800

    chore: change build config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,59 @@
+'use strict'
+const path = require('path')
+
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+const isDev = process.env.NODE_ENV === 'development'
+
 const vueConfig = {
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
     // webpack plugins
     plugins: [
       Components({
         resolvers: [ElementPlusResolver()],
       }),
     ],
+  },
+  chainWebpack(config) {
+    // when there are many pages, it will cause too many meaningless requests
+    config.plugins.delete('prefetch')
+
+    config.when(!isDev, (config) => {
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial', // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-elementPlus', // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-plus(.*)/, // in order to adapt to cnpm
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: resolve('src/components'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      })
+      // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+      config.optimization.runtimeChunk('single')
+    })
   },
 }
 
